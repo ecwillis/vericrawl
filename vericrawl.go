05d9a05f55c794a31e97306764b94af9e5f704f1
Eric Willis <eric@ecwillis.com>
2019-02-01 21:36:42 +0000

    Added invalid HTML checker.

--- vericrawl.go
+++ vericrawl.go
@@ -7,6 +7,7 @@ import (
     "io/ioutil"
     "strings"
     "time"
+    "regexp"
     . "github.com/logrusorgru/aurora"
 )
 
@@ -22,48 +23,77 @@ func main() {
     fmt.Printf("Reading %s",mainUrl)
     fmt.Println("")
 
-    resp, err := http.Get(mainUrl)
+
+    // Client based w/UserAgent
+    client := &http.Client{}
+
+    req, err := http.NewRequest("GET", mainUrl, nil)
+    if err != nil {
+      panic(err)
+    }
+    req.Header.Set("User-Agent", "F+T VeriCrawl bot/1.0")
+
+    resp, err := client.Do(req)
 
     if err != nil {
-        fmt.Println("Error!")
-        return
+        panic(err)
     }
 
     defer resp.Body.Close()
 
     body, err := ioutil.ReadAll(resp.Body)
-
     if err != nil {
-        fmt.Println("Body Error!")
-        return
+        panic(err)
     }
-    bodyStr := string(body)
 
+    bodyStr := string(body)
     crawlCache := strings.Split(bodyStr, "\n")
 
     duration := time.Duration(300) * time.Millisecond
-
+    var badHtmlTest = regexp.MustCompile(`&lt;.*&gt;`)
+    var okHtmlTest = regexp.MustCompile(`[a-z-]*=".*&lt;.*&gt;.*"`)
     for _, url := range crawlCache {
         plainUrl := strings.Replace(url, "https", "http", 1)
-        resp, err := http.Head(plainUrl)
+        subReq, err := http.NewRequest("GET", plainUrl, nil)
+        subReq.Header.Set("User-Agent", "F+T VeriCrawl bot/1.0")
+
+        subResp, err := client.Do(subReq)
+
         if err != nil {
             panic(err)
         }
-        if resp.StatusCode == 200 {
-            fmt.Println(Green(resp.StatusCode), plainUrl)
+
+        defer subResp.Body.Close()
+
+        subBody, err := ioutil.ReadAll(subResp.Body)
+
+        badData := badHtmlTest.FindIndex(subBody)
+        okData := okHtmlTest.FindIndex(subBody)
+
+        htmlValid := Green("true ")
+        if len(badData) > 0 && len(okData) != len(badData) {
+          htmlValid = Red("false")
         }
 
-        if resp.StatusCode < 399 && resp.StatusCode > 200 {
-            fmt.Println(Brown(resp.StatusCode), plainUrl)
+        if err != nil {
+          panic(err)
         }
 
-        if resp.StatusCode > 399 && resp.StatusCode < 500 {
-            fmt.Println(Magenta(resp.StatusCode), plainUrl)
+        retCode := Green(subResp.StatusCode)
+
+        if subResp.StatusCode < 399 && subResp.StatusCode > 200 {
+            retCode = Brown(subResp.StatusCode)
         }
 
-        if resp.StatusCode > 499 {
-            fmt.Println(Red(resp.StatusCode), plainUrl)
+        if subResp.StatusCode > 399 && subResp.StatusCode < 500 {
+            retCode = Magenta(subResp.StatusCode)
         }
+
+        if subResp.StatusCode > 499 {
+            retCode = Red(subResp.StatusCode)
+        }
+
+        fmt.Println(retCode, htmlValid, url)
         time.Sleep(duration)
     }
 }
